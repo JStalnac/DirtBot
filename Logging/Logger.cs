39afdeb9c8cc279d2cfeb0e5d47dfe0391499542
JStalnac <55840834+JStalnac@users.noreply.github.com>
2019-11-15 12:19:41 +0200

    Added the Logging namespace
    - Added Logger.cs
    - Added LogSeverity.cs
    - Basicly copied from Discord.Net

---
+++ Logging/Logger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SmartFormat;
+
+namespace DirtBot.Logging
+{
+
+    internal class Logger
+    {
+        string source;
+        LogSeverity logSeverity;
+
+        public Logger(string source, LogSeverity severity)
+        {
+            this.source = source;
+            logSeverity = severity;
+        }
+
+        public string Source
+        {
+            get { return source; }
+            set { source = value; }
+        }
+
+        public async Task LogAsync(LogSeverity severity, string message)
+        {
+            if (severity <= logSeverity)
+                Console.WriteLine("[ {2} | {0} {1} ]: {3}".FormatSmart(DateTime.Now.ToString("dd'/'MM'/'yy H:mm:ss"), source, severity, message));
+        }
+
+        public Task CriticalAsync(string message)
+            => LogAsync(LogSeverity.Critical, message);
+
+        public Task ErrorAsync(string message)
+            => LogAsync(LogSeverity.Error, message);
+
+        public Task WarningAsync(string message)
+            => LogAsync(LogSeverity.Warning, message);
+
+        public Task InfoAsync(string message)
+            => LogAsync(LogSeverity.Info, message);
+
+        public Task VerboseAsync(string message)
+            => LogAsync(LogSeverity.Verbose, message);
+
+        public Task DebugAsync(string message)
+            => LogAsync(LogSeverity.Debug, message);
+
+        public static Logger GetLogger(object logger)
+        {
+            return new Logger(logger.GetType().Name, LogSeverity.Debug);
+        }
+
+        public static Logger GetLogger(string source)
+        {
+            return new Logger(source, LogSeverity.Debug);
+        }
+
+        public static Logger GetLogger(object logger, LogSeverity severity)
+        {
+            return new Logger(logger.GetType().Name, severity);
+        }
+
+        public static Logger GetLogger(string source, LogSeverity severity)
+        {
+            return new Logger(source, severity);
+        }
+    }
+}
