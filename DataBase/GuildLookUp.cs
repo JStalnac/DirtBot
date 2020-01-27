83d0e0f7faea180e14ea8d76a3e58a362ddfc229
JStalnac <55840834+JStalnac@users.noreply.github.com>
2020-01-27 20:28:19 +0200

    Trying to implement some kind of smarter storage

---
+++ DataBase/GuildLookUp.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DirtBot.DataBase
+{
+    public class GuildLookUp<TKey, TValue> : LookupTable<TKey, TValue>
+    {
+        public override string FileName => throw new NotImplementedException();
+    }
+}
