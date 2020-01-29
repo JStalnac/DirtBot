d1a8ea79543002f95b48118eb9e7803081c20d75
JStalnac <55840834+JStalnac@users.noreply.github.com>
2020-01-29 20:31:21 +0200

    Create GuildDataLookUp.cs

---
+++ DataBase/GuildDataLookUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirtBot.DataBase
+{
+    public class GuildDataLookUp : LookupTable<ulong, object>
+    {
+        public override string FileName => throw new Exception();
+    }
+}
