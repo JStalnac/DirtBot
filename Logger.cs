8535267bea249d2af2edc2f77655e929cdc8952f
JStalnac <55840834+JStalnac@users.noreply.github.com>
2020-10-14 18:23:58 +0300

    Update logger

--- Logger.cs
+++ Logger.cs
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
+using Pastel;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
-using System.Text.RegularExpressions;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Pastel;
 
-namespace DirtBot
+namespace JStalnac.Common.Logging
 {
     /// <summary>
     /// Log level that the logger uses.
@@ -65,15 +65,74 @@ namespace DirtBot
 
     public sealed class Logger
     {
-        private readonly string name;
-        private static LogLevel level = LogLevel.Info;
+        /// <summary>
+        /// Gets or sets the minumum log level for the loggers.
+        /// </summary>
+        public static LogLevel MinimumLogLevel { get; set; } = LogLevel.Info;
 
+        /// <summary>
+        /// Gets or sets the log file path that logs will be written into.
+        /// </summary>
+        public static string LogFile
+        {
+            get => logFile;
+            set
+            {
+                if (value == null)
+                {
+                    logFile = null;
+                    return;
+                }
+
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(nameof(value));
+
+                try
+                {
+                    // This has flaws
+                    Path.GetFullPath(value);
+                }
+                catch
+                {
+                    throw new ArgumentException("Invalid path", nameof(value));
+                }
+
+                logFile = value;
+            }
+        }
         private static string logFile;
 
+        /// <summary>
+        /// Gets or sets the datetime format the loggers use.
+        /// </summary>
+        public static string DateTimeFormat
+        {
+            get => datetimeFormat;
+            set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException(nameof(value));
+
+                try
+                {
+                    DateTime.Now.ToString(value);
+                }
+                catch (FormatException)
+                {
+                    throw new FormatException("Invalid datetime format");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    throw new FormatException("Invalid datetime");
+                }
+
+                datetimeFormat = value;
+            }
+        }
         // I'm not American
         private static string datetimeFormat = "dd/MM/yyyy HH:mm:ssZzzz";
 
-        private static readonly Regex nameRegex = new Regex("[\x00-\x1F\x7F]", RegexOptions.Compiled);
+        private readonly string name;
 
         /// <summary>
         /// Initializes a new instance of the Logger class.
@@ -82,11 +141,52 @@ namespace DirtBot
         public Logger(string name)
         {
             // Sanitize the input
-            string cleanName = nameRegex.Replace(name, "");
+            string cleanName = Regex.Replace(name, "[\x00-\x1F\x7F]", "");
             if (String.IsNullOrEmpty(cleanName) || String.IsNullOrWhiteSpace(cleanName))
                 throw new ArgumentNullException(nameof(name));
             this.name = cleanName;
         }
+
+        /// <summary>
+        /// Sets the log file used. If no file is set messages won't be logged to a file.
+        /// </summary>
+        /// <param name="path"></param>
+        [Obsolete]
+        public static void SetLogFile(string path) => LogFile = path;
+
+        /// <summary>
+        /// Sets the minimum log level. The default level is <see cref="LogLevel.Info"/>
+        /// </summary>
+        /// <param name="logLevel"></param>
+        [Obsolete]
+        public static void SetLogLevel(LogLevel logLevel) => MinimumLogLevel = logLevel;
+
+        /// <summary>
+        /// Sets the datetime format used.
+        /// </summary>
+        /// <param name="format"></param>
+        [Obsolete]
+        public static void SetDateTimeFormat(string format) => DateTimeFormat = format;
+
+        /// <summary>
+        /// Gets a new logger for the specified type.
+        /// </summary>
+        /// <returns></returns>
+        public static Logger GetLogger<T>(T type) => new Logger(typeof(T).Name);
+
+        /// <summary>
+        /// Gets a new logger for the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Logger GetLogger<T>() => new Logger(typeof(T).Name);
+
+        /// <summary>
+        /// Gets a new logger with the specified name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Logger GetLogger(string name) => new Logger(name);
 
         [DllImport("kernel32.dll")]
         private static extern bool AllocConsole();
@@ -103,257 +203,118 @@ namespace DirtBot
         }
 
         /// <summary>
-        /// Sets the log file used. If no file is set messages won't be logged to a file.
-        /// </summary>
-        /// <param name="path"></param>
-        public static void SetLogFile(string path)
-        {
-            if (String.IsNullOrEmpty(path) || String.IsNullOrWhiteSpace(path))
-                throw new ArgumentNullException(nameof(path));
-
-            try
-            {
-                // This has flaws
-                Path.GetFullPath(path);
-            }
-            catch
-            {
-                throw new ArgumentException("Invalid path", nameof(path));
-            }
-
-            logFile = path;
-        }
-
-        /// <summary>
-        /// Sets the minimum log level. The default level is <see cref="LogLevel.Info"/>
-        /// </summary>
-        /// <param name="logLevel"></param>
-        public static void SetLogLevel(LogLevel logLevel)
-        {
-            level = logLevel;
-        }
-
-        /// <summary>
-        /// Sets the datetime format used.
-        /// </summary>
-        /// <param name="format"></param>
-        public static void SetDateTimeFormat(string format)
-        {
-            if (String.IsNullOrEmpty(format) || String.IsNullOrWhiteSpace(format))
-                throw new ArgumentException(nameof(format));
-
-            try
-            {
-                DateTime.Now.ToString(format);
-            }
-            catch (FormatException)
-            {
-                throw new FormatException("Invalid datetime format");
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                throw new FormatException("Invalid datetime");
-            }
-
-            datetimeFormat = format;
-        }
-
-        /// <summary>
-        /// Gets a new logger for the specified type.
-        /// </summary>
-        /// <returns></returns>
-        public static Logger GetLogger<T>(T type)
-        {
-            return new Logger(typeof(T).Name);
-        }
-
-        /// <summary>
-        /// Gets a new logger for the specified type.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static Logger GetLogger<T>()
-        {
-            return new Logger(typeof(T).Name);
-        }
-
-        /// <summary>
-        /// Gets a new logger with the specified name.
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public static Logger GetLogger(string name)
-        {
-            return new Logger(name);
-        }
-
-        /// <summary>
         /// Writes a log message on <see cref="LogLevel.Debug"/> log level.
         /// </summary>
         /// <param name="message">Log message</param>
-        public void Debug(string message)
-        {
-            Write(message, LogLevel.Debug);
-        }
+        public void Debug(string message) => Write(message, LogLevel.Debug);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Debug"/> log level including an exception.
         /// </summary>
         /// <param name="message">Log message</param>
         /// <param name ="e">Exception</param>
-        public void Debug(string message, Exception e)
-        {
-            Write(message, LogLevel.Debug, e);
-        }
+        public void Debug(string message, Exception e) => Write(message, LogLevel.Debug, e);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Debug"/> log level using the provided object's <see cref="System.Object.ToString"/> method.
         /// </summary>
         /// <param name="obj">Object</param>
-        public void Debug(object obj)
-        {
-            Write(obj, LogLevel.Debug);
-        }
+        public void Debug(object obj) => Write(obj, LogLevel.Debug);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Info"/> log level.
         /// </summary>
         /// <param name="message">Log message</param>
-        public void Info(string message)
-        {
-            Write(message, LogLevel.Info);
-        }
+        public void Info(string message) => Write(message, LogLevel.Info);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Info"/> log level including an exception.
         /// </summary>
         /// <param name="message">Log message</param>
         /// <param name ="e">Exception</param>
-        public void Info(string message, Exception e)
-        {
-            Write(message, LogLevel.Info, e);
-        }
+        public void Info(string message, Exception e) => Write(message, LogLevel.Info, e);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Info"/> log level using the provided object's <see cref="System.Object.ToString"/> method.
         /// </summary>
         /// <param name="obj">Object</param>
-        public void Info(object obj)
-        {
-            Write(obj, LogLevel.Info);
-        }
+        public void Info(object obj) => Write(obj, LogLevel.Info);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Warning"/> log level.
         /// </summary>
         /// <param name="message">Log message</param>
-        public void Warning(string message)
-        {
-            Write(message, LogLevel.Warning);
-        }
+        public void Warning(string message) => Write(message, LogLevel.Warning);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Warning"/> log level including an exception.
         /// </summary>
         /// <param name="message">Log message</param>
         /// <param name ="e">Exception</param>
-        public void Warning(string message, Exception e)
-        {
-            Write(message, LogLevel.Warning, e);
-        }
+        public void Warning(string message, Exception e) => Write(message, LogLevel.Warning, e);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Warning"/> log level using the provided object's <see cref="System.Object.ToString"/> method.
         /// </summary>
         /// <param name="obj">Object</param>
-        public void Warning(object obj)
-        {
-            Write(obj, LogLevel.Warning);
-        }
+        public void Warning(object obj) => Write(obj, LogLevel.Warning);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Error"/> log level.
         /// </summary>
         /// <param name="message">Log message</param>
-        public void Error(string message)
-        {
-            Write(message, LogLevel.Error);
-        }
+        public void Error(string message) => Write(message, LogLevel.Error);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Error"/> log level including an exception.
         /// </summary>
         /// <param name="message">Log message</param>
         /// <param name ="e">Exception</param>
-        public void Error(string message, Exception e)
-        {
-            Write(message, LogLevel.Error, e);
-        }
+        public void Error(string message, Exception e) => Write(message, LogLevel.Error, e);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Error"/> log level using the provided object's <see cref="System.Object.ToString"/> method.
         /// </summary>
         /// <param name="obj">Object</param>
-        public void Error(object obj)
-        {
-            Write(obj, LogLevel.Error);
-        }
+        public void Error(object obj) => Write(obj, LogLevel.Error);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Important"/> log level.
         /// </summary>
         /// <param name="message">Log message</param>
-        public void Important(string message)
-        {
-            Write(message, LogLevel.Important);
-        }
+        public void Important(string message) => Write(message, LogLevel.Important);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Important"/> log level including an exception.
         /// </summary>
         /// <param name="message">Log message</param>
         /// <param name ="e">Exception</param>
-        public void Important(string message, Exception e)
-        {
-            Write(message, LogLevel.Important, e);
-        }
+        public void Important(string message, Exception e) => Write(message, LogLevel.Important, e);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Important"/> log level using the provided object's <see cref="System.Object.ToString"/> method.
         /// </summary>
         /// <param name="obj">Object</param>
-        public void Important(object obj)
-        {
-            Write(obj, LogLevel.Important);
-        }
+        public void Important(object obj) => Write(obj, LogLevel.Important);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Critical"/> log level.
         /// </summary>
         /// <param name="message">Log message</param>
-        public void Critical(string message)
-        {
-            Write(message, LogLevel.Critical);
-        }
+        public void Critical(string message) => Write(message, LogLevel.Critical);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Critical"/> log level including an exception.
         /// </summary>
         /// <param name="message">Log message</param>
         /// <param name ="e">Exception</param>
-        public void Critical(string message, Exception e)
-        {
-            Write(message, LogLevel.Critical, e);
-        }
+        public void Critical(string message, Exception e) => Write(message, LogLevel.Critical, e);
 
         /// <summary>
         /// Writes a log message on <see cref="LogLevel.Critical"/> log level using the provided object's <see cref="System.Object.ToString"/> method.
         /// </summary>
         /// <param name="obj">Object</param>
-        public void Critical(object obj)
-        {
-            Write(obj, LogLevel.Critical);
-        }
+        public void Critical(object obj) => Write(obj, LogLevel.Critical);
 
         private static Color GetColor(LogLevel logLevel)
         {
@@ -368,6 +329,34 @@ namespace DirtBot
                 _ => Color.LightGray
             };
         }
+
+        /// <summary>
+        /// Writes a log message using an object's <see cref="System.Object.ToString"/> method.
+        /// </summary>
+        /// <param name="obj">Object</param>
+        /// <param name="logLevel">Log level</param>
+        public void Write(object obj, LogLevel logLevel) => Write(obj?.ToString(), logLevel, null);
+
+        /// <summary>
+        /// Writes a log message using the provided log level including an exception.
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="logLevel">Log level</param>
+        /// <param name="exception">Exception</param>
+        public void Write(string message, LogLevel logLevel, Exception exception = null)
+        {
+            if (MinimumLogLevel > logLevel)
+                return;
+            Write(message, GetColor(logLevel), exception: exception, logLevel: logLevel);
+        }
+
+        /// <summary>
+        /// Writes a log message in a custom color using an object's <see cref="System.Object.ToString"/> method.
+        /// </summary>
+        /// <param name="obj">Object</param>
+        /// <param name="fore">Foreground color</param>
+        /// <param name="back">Background color</param>
+        public void Write(object obj, Color fore, Color? back = null) => Write(obj?.ToString(), fore, back);
 
         private static object writeLock = new object();
 
@@ -438,39 +427,6 @@ namespace DirtBot
                 // End write to file
                 fileWrite?.Wait();
             }
-        }
-
-        /// <summary>
-        /// Writes a log message using the provided log level including an exception.
-        /// </summary>
-        /// <param name="message">Message</param>
-        /// <param name="logLevel">Log level</param>
-        /// <param name="exception">Exception</param>
-        public void Write(string message, LogLevel logLevel, Exception exception = null)
-        {
-            if (level <= logLevel)
-                Write(message, GetColor(logLevel), exception: exception, logLevel: logLevel);
-        }
-
-        /// <summary>
-        /// Writes a log message using an object's <see cref="System.Object.ToString"/> method.
-        /// </summary>
-        /// <param name="obj">Object</param>
-        /// <param name="logLevel">Log level</param>
-        public void Write(object obj, LogLevel logLevel)
-        {
-            Write(obj?.ToString(), logLevel, null);
-        }
-
-        /// <summary>
-        /// Writes a log message in a custom color using an object's <see cref="System.Object.ToString"/> method.
-        /// </summary>
-        /// <param name="obj">Object</param>
-        /// <param name="fore">Foreground color</param>
-        /// <param name="back">Background color</param>
-        public void Write(object obj, Color fore, Color? back = null)
-        {
-            Write(obj?.ToString(), fore, back);
         }
     }
 }
